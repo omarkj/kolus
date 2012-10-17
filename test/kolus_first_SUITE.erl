4e0fa79fff1be894e295ab472c6017277362e98d
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-10-17 11:31:30 -0700

    Removed the old tuple in favour of the new record when matching on backends.

--- test/kolus_first_SUITE.erl
+++ test/kolus_first_SUITE.erl
@@ -1,5 +1,6 @@
 -module(kolus_first_SUITE).
 -include_lib("common_test/include/ct.hrl").
+-include("../include/kolus.hrl").
 -include("../src/kolus_internal.hrl").
 
 -export([all/0]).
@@ -48,20 +49,22 @@ end_per_suite(Config) ->
 no_managers(Config) ->
     Port = ?config(open_port, Config),
     Backends = ?config(backends, Config),
-    [] = kolus:status(Backends),
-    {socket, KSocket} = kolus:connect(<<"test">>, hd(Backends)),
+    [B=#kolus_backend{},#kolus_backend{}] = kolus:status(Backends),
+    {socket, KSocket} = kolus:connect(<<"test">>, B),
     case erlang:port_info(kolus:get_socket(KSocket)) of
 	A when is_list(A) ->
 	    ok
     end,
     % Check unused, should be 9
-    [{{{127,0,0,1},Port},_Pid,{idle,0},{unused,9}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		   unused=9,idle=0} = hd(kolus:status(Backends)),
     % Return socket
     ok = kolus:return(KSocket),
     % Need to wait for the insert to finish, do it somewhere else - this is needed
     % for the test, not in regular usage.
     timer:sleep(1),
-    [{{{127,0,0,1},Port},_Pid,{idle,1},{unused,9}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		   unused=9,idle=1} = hd(kolus:status(Backends)),
     {save_config, [{old_socket,KSocket}]}.
 
 % Get the idle socket created in the test above
@@ -69,36 +72,43 @@ get_idle(Config) ->
     Backends = ?config(backends, Config),
     {_, SavedConfig} = ?config(saved_config, Config),
     OldSocket = proplists:get_value(old_socket, SavedConfig),
-    [{{{127,0,0,1},Port},Pid,{idle,1},{unused,9}}] = kolus:status(Backends),
-    {socket, KSocket} = kolus:connect(<<"test">>, Pid),
+    B = #kolus_backend{ip={127,0,0,1},port=Port,
+		       unused=9,idle=1} = hd(kolus:status(Backends)),
+    {socket, KSocket} = kolus:connect(<<"test">>, B),
     true = kolus:get_socket(OldSocket) == kolus:get_socket(KSocket),
     timer:sleep(1),
-    [{{{127,0,0,1},Port},_Pid,{idle,0},{unused,9}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		   unused=9,idle=0} = hd(kolus:status(Backends)),
     ok = kolus:return(KSocket),
     timer:sleep(1),
-    [{{{127,0,0,1},Port},_Pid,{idle,1},{unused,9}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		   unused=9,idle=1} = hd(kolus:status(Backends)),
     Config.
 
 % Return unusable socket
 return_unusable(Config) ->
     Backends = ?config(backends, Config),
-    [{{{127,0,0,1},Port},Pid,{idle,1},{unused,9}}] = kolus:status(Backends),
-    {socket, KSocket} = kolus:connect(<<"test">>, Pid),
+    B = #kolus_backend{ip={127,0,0,1},port=Port,
+		       unused=9, idle=1} = hd(kolus:status(Backends)),
+    {socket, KSocket} = kolus:connect(<<"test">>, B),
     gen_tcp:close(kolus:get_socket(KSocket)),
     ok = kolus:finished(KSocket),
     timer:sleep(1),
-    [{{{127,0,0,1},Port},Pid,{idle,0},{unused,10}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		   unused=10} = hd(kolus:status(Backends)),
     Config.
 
 % A socket times out that's being held by the manager
 socket_timeout(Config) ->
     Backends = ?config(backends, Config),
     ok = application:set_env(kolus, socket_idle_timeout, 100),
-    [{{{127,0,0,1},_Port},Pid,{idle,0},{unused,10}}] = kolus:status(Backends),
-    {socket, KSocket} = kolus:connect(<<"test">>, Pid),
+    B = #kolus_backend{ip={127,0,0,1},port=Port,
+		       unused=10} = hd(kolus:status(Backends)),
+    {socket, KSocket} = kolus:connect(<<"test">>, B),
     ok = kolus:return(KSocket),
     timer:sleep(101),
-    [{{{127,0,0,1},_Port},Pid,{idle,0},{unused,10}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		   unused=10} = hd(kolus:status(Backends)),
     ok = application:set_env(kolus, socket_idle_timeout, 5000),
     Config.
 
@@ -108,10 +118,11 @@ manager_timeout(Config) ->
     ok = application:set_env(kolus, socket_idle_timeout, 100),
     ok = application:set_env(kolus, manager_idle_timeout, 100),
     % Start by shutting down the current manager
-    [{{{127,0,0,1},_Port},Pid,{idle,0},{unused,10}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		       manager=Pid,unused=10} = hd(kolus:status(Backends)),
     ok = kolus_manager:stop_sync(Pid),
-    [] = kolus:status(Backends),
-    {socket, KSocket} = kolus:connect(<<"test">>, hd(Backends)),
+    [B,_] = kolus:status(Backends),
+    {socket, KSocket} = kolus:connect(<<"test">>, B),
     MngrPid = kolus:get_manager(KSocket),
     % Wait here for the manager idle time, nothing should happen
     timer:sleep(101),
@@ -132,7 +143,8 @@ full_manager(Config) ->
     ok = application:set_env(kolus, endpoint_connection_limit, 2),
     {socket, KSocket} = kolus:connect(<<"test">>, hd(Backends)),
     {socket, KSocket1} = kolus:connect(<<"test">>, hd(Backends)),
-    [{{{127,0,0,1},_Port},_Pid,{idle,0},{unused,0}}] = kolus:status(Backends),
+    #kolus_backend{ip={127,0,0,1},port=Port,
+		   unused=0,idle=0} = hd(kolus:status(Backends)),
     {error, rejected} = kolus:connect(<<"test">>, hd(Backends)),
     ok = kolus:return(KSocket),
     {socket, KSocket2} = kolus:connect(<<"test">>, hd(Backends)),
@@ -162,8 +174,10 @@ fill_first(Config) ->
     B1 = {L, Port1},
     B2 = {L, Port2},
     Backends = [B0,B1,B2],
-    % No backends exist
-    [] = kolus:status(Backends),
+    % No active backends exist
+    [#kolus_backend{manager=undefined},
+     #kolus_backend{manager=undefined},
+     #kolus_backend{manager=undefined}] = kolus:status(Backends),
     % To make this test slighty more annoying I'm going to connect
     % to a few sockets on all the backends. This distribution means
     % that B0 should be filled up first, then B1, then B2.
@@ -174,23 +188,23 @@ fill_first(Config) ->
     {socket, _} = kolus:connect(<<"test">>, B0),
     {socket, _} = kolus:connect(<<"test">>, B0),
     % Start by checking the status of all the managers
-    [{{{127,0,0,1},Port0},_,{idle,0},{unused,A}},
-     {{{127,0,0,1},Port1},_,{idle,0},{unused,B}},
-     {{{127,0,0,1},Port2},_,{idle,0},{unused,C}}] = kolus:status(Backends),
+    [#kolus_backend{ip={127,0,0,1},port=Port0,idle=0,unused=A},
+     #kolus_backend{ip={127,0,0,1},port=Port1,idle=0,unused=B},
+     #kolus_backend{ip={127,0,0,1},port=Port2,idle=0,unused=C}] = kolus:status(Backends),
     % Fill up the sockets. This is going on on a single process and does take
     % a while.
     ok = fill_backend(Backends, A),
-    [{{{127,0,0,1},Port0},_,{idle,0},{unused,0}},
-     {{{127,0,0,1},Port1},_,{idle,0},{unused,B}},
-     {{{127,0,0,1},Port2},_,{idle,0},{unused,C}}] = kolus:status(Backends),
+    [#kolus_backend{ip={127,0,0,1},port=Port0,idle=0,unused=0},
+     #kolus_backend{ip={127,0,0,1},port=Port1,idle=0,unused=B},
+     #kolus_backend{ip={127,0,0,1},port=Port2,idle=0,unused=C}] = kolus:status(Backends),
     ok = fill_backend(Backends, B),
-    [{{{127,0,0,1},Port0},_,{idle,0},{unused,0}},
-     {{{127,0,0,1},Port1},_,{idle,0},{unused,0}},
-     {{{127,0,0,1},Port2},_,{idle,0},{unused,C}}] = kolus:status(Backends),
+    [#kolus_backend{ip={127,0,0,1},port=Port0,idle=0,unused=0},
+     #kolus_backend{ip={127,0,0,1},port=Port1,idle=0,unused=0},
+     #kolus_backend{ip={127,0,0,1},port=Port2,idle=0,unused=C}] = kolus:status(Backends),
     ok = fill_backend(Backends, C),
-    [{{{127,0,0,1},Port0},_,{idle,0},{unused,0}},
-     {{{127,0,0,1},Port1},_,{idle,0},{unused,0}},
-     {{{127,0,0,1},Port2},_,{idle,0},{unused,0}}] = kolus:status(Backends),
+    [#kolus_backend{ip={127,0,0,1},port=Port0,idle=0,unused=0},
+     #kolus_backend{ip={127,0,0,1},port=Port1,idle=0,unused=0},
+     #kolus_backend{ip={127,0,0,1},port=Port2,idle=0,unused=0}] = kolus:status(Backends),
     Config.
 
 % Internal
@@ -201,10 +215,9 @@ fill_backend(Backends, Runs) ->
     fill_backend(Backends, Runs-1).
 
 ff_filter(Backends) ->
-    {Backend,_,_,_} = 
 	hd(lists:sort(fun
-			  ({_,_,{idle,A},{unused,X}},
-			   {_,_,{idle,B},{unused,Y}}) ->
+			  (#kolus_backend{idle=A,unused=X},
+			   #kolus_backend{idle=B,unused=Y}) ->
 			      case A > B of
 				  true ->
 				      true;
@@ -216,8 +229,7 @@ ff_filter(Backends) ->
 					      X =< Y
 				      end
 			      end
-		      end, Backends)),
-    Backend.
+		      end, Backends)).
 
 start_application(App) ->
     case application:start(App) of
