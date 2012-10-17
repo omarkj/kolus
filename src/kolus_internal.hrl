f1808aba3d67fa277335e9d1aee95e4bfa1f4957
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-10-17 16:04:40 -0700

    Add the manager key, that key is used by gproc when registering a new manager.

--- src/kolus_internal.hrl
+++ src/kolus_internal.hrl
@@ -1,5 +1,7 @@
 -define(INFO_MSG(Msg), error_logger:info_msg(Msg)).
 -define(INFO_MSG(Format, Msg), error_logger:info_msg(Format, Msg)).
 
+-define(MANAGER_KEY(Ip, Port), {n,l,{manager,Ip,Port}}).
+
 -define(LOOKUP_TID(Key), {n,l,{backend_tid,Key}}).
 -define(LOOKUP_PID(Key), {n,l,{backend_pid,Key}}).
