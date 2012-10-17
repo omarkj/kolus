45b238d60f0b5684fde03b8b0033c9c3a4368c9b
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-10-16 17:03:38 -0700

    Add a test to test timeout for managers.

--- test/kolus_first_SUITE.erl
+++ test/kolus_first_SUITE.erl
@@ -10,14 +10,16 @@
 -export([no_managers/1,
 	 get_idle/1,
 	 return_unusable/1,
-	 socket_timeout/1
+	 socket_timeout/1,
+	 manager_timeout/1
 	]).
 
 all() ->
     [no_managers
      ,get_idle
      ,return_unusable
-     ,socket_timeout].
+     ,socket_timeout
+     ,manager_timeout].
 
 % Setup & teardown
 init_per_suite(Config) ->
@@ -90,7 +92,30 @@ socket_timeout(Config) ->
     ok = kolus:return(KSocket),
     timer:sleep(101),
     [{{{127,0,0,1},_Port},Pid,[{idle,0},{unused,10}]}] = kolus:status(Backends),
-    ok.
+    ok = application:set_env(kolus, socket_idle_timeout, 5000),
+    Config.
+
+% A manager times out
+manager_timeout(Config) ->
+    Backends = ?config(backends, Config),
+    ok = application:set_env(kolus, socket_idle_timeout, 100),
+    ok = application:set_env(kolus, manager_idle_timeout, 100),
+    % Start by shutting down the current manager
+    [{{{127,0,0,1},_Port},Pid,[{idle,0},{unused,10}]}] = kolus:status(Backends),
+    ok = kolus_manager:stop_sync(Pid),
+    [] = kolus:status(Backends),
+    {socket, KSocket} = kolus:connect(<<"test">>, hd(Backends)),
+    MngrPid = kolus:get_manager(KSocket),
+    % Wait here for the manager idle time, nothing should happen
+    timer:sleep(101),
+    true = erlang:is_process_alive(MngrPid),
+    % Return the socket, after the socket timeout (100ms) + the manager
+    % timeout (100ms) the manager will be history. A bit of grace period
+    % is also nice.
+    ok = kolus:return(KSocket),
+    timer:sleep(250),
+    false = erlang:is_process_alive(MngrPid),
+    Config.
 
 % Internal
 start_application(App) ->
@@ -104,7 +129,6 @@ start_application(App) ->
 
 start_server() ->
     {ok, Socket} = gen_tcp:listen(0, [{active,false}]),
-    
     {ok, PortNo} = inet:port(Socket),
     Server = spawn(fun() -> listener() end),
     gen_tcp:controlling_process(Socket, Server),
