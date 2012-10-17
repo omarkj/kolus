66a36d17f0478f98dcdda53944f1efe96e68ece7
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-10-17 15:37:57 -0700

    Add the new gproc keys for counting the idle and unused sockets.

--- src/kolus_internal.hrl
+++ src/kolus_internal.hrl
@@ -1,5 +1,7 @@
 -define(INFO_MSG(Msg), error_logger:info_msg(Msg)).
 -define(INFO_MSG(Format, Msg), error_logger:info_msg(Format, Msg)).
 
--define(LOOKUP_TID(Key), {n,l,{backend_tid,Key}}).
+-define(IDLE_KEY(Key), {c,l,{idle,Key}}).
+-define(UNUSED_KEY(Key), {c,l,{unused,Key}}).
+
 -define(LOOKUP_PID(Key), {n,l,{backend_pid,Key}}).
